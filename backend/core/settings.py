09891807f22c43bb6ed55a45efd7ff1e000e6897
Jeyasurya14 <jeyas865@gmail.com>
2026-01-13 03:56:05 +0530

    chore: enable postgresql support

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -120,11 +120,13 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # MongoDB Config
